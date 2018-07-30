44c62070fc23347e038f4b6980c9be4da81da5c5
Ethan <dengxudong@feinno.com>
2018-07-30 17:20:46 +0800

    IntenrServer详解

--- app/src/main/java/com/example/layout/layoutdemo/MainActivity.java
+++ app/src/main/java/com/example/layout/layoutdemo/MainActivity.java
@@ -5,12 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.layout.layoutdemo.utils.IntentServerDemo;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Intent intent = new Intent(MainActivity.this, IntentServerDemo.class);
+//        intent.putExtra("action","TASK1");
+//// 开启第一个服务
+//        startService(intent);
+//        intent.putExtra("action","TASK2");
+//// 开启第二个服务
+//        startService(intent);
+//        intent.putExtra("action","TASK3");
+//// 开启第三个服务
+//        startService(intent);
     }
 
     public void clic1(View view){
